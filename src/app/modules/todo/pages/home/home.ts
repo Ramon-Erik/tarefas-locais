75d37b100803516d6c81b76d147e2bbae7de1d3f
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-07-25 15:21:09 -0300

    feat: empty task component

--- src/app/modules/todo/pages/home/home.ts
+++ src/app/modules/todo/pages/home/home.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { EmptyTask } from "../../components/empty-task/empty-task";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [EmptyTask],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-
+  public tasks = localStorage.getItem('tasks')
 }
