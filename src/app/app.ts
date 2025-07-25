e6d985db043aef6a07604cecb8257fab7cac8967
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-07-25 13:10:09 -0300

    feat(todo): reseting html file

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('tarefas-locais');
+  protected readonly title = signal('Local Todo');
 }
