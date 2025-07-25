e91153a29bd28bfb4b7f6e393a6f419d1bd81a6b
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2025-07-25 19:05:26 -0300

    feat: changed img to home

--- src/app/modules/todo/pages/home/home.ts
+++ src/app/modules/todo/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { EmptyTask } from "../../components/empty-task/empty-task";
+import { AddNewTask } from "../../components/add-new-task/add-new-task";
 
 @Component({
   selector: 'app-home',
-  imports: [EmptyTask],
+  imports: [EmptyTask, AddNewTask],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
